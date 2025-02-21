b7f5472c6f5024c06766b3c1d0567585d5838bf1
ptrnk0 <vlad.ptrnko@gmail.com>
2025-02-21 12:31:13 +0200

    Implement writeContacts function to append updated contacts to the database

--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,15 @@
 import { PATH_DB } from '../constants/contacts.js';
+import * as fs from 'node:fs/promises';
+import { readContacts } from './readContacts.js';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+  try {
+    const contacts = await readContacts();
+    await fs.writeFile(
+      PATH_DB,
+      JSON.stringify([...contacts, ...updatedContacts]),
+    );
+  } catch (error) {
+    console.error(error);
+  }
+};
