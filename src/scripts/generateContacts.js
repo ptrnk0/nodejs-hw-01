d995cb8e35af4fca69900466808fb52c3356b320
ptrnk0 <vlad.ptrnko@gmail.com>
2025-02-21 12:58:17 +0200

    Update generateContacts to read existing contacts and append new fake contacts

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,13 +1,16 @@
 import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
 import { writeContacts } from '../utils/writeContacts.js';
 
 const generateContacts = async (number) => {
+  const contacts = await readContacts();
   const newContacts = [];
+
   for (let index = 0; index < number; index++) {
     newContacts.push(createFakeContact());
   }
 
-  await writeContacts(newContacts);
+  await writeContacts([...contacts, ...newContacts]);
 };
 
 generateContacts(5);
