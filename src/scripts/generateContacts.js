2633bbc304919c4305c03f33dfd9153739983edd
ptrnk0 <vlad.ptrnko@gmail.com>
2025-02-21 12:30:59 +0200

    Implement generateContacts function to create and write multiple fake contacts

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,13 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+const generateContacts = async (number) => {
+  const newContacts = [];
+  for (let index = 0; index < number; index++) {
+    newContacts.push(createFakeContact());
+  }
+
+  await writeContacts(newContacts);
+};
 
 generateContacts(5);
