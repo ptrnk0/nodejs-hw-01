bdad386eb3d633dd265888be089bb7647bcc5e15
ptrnk0 <vlad.ptrnko@gmail.com>
2025-02-21 11:54:13 +0200

    Implement readContacts function to read and parse contact data from file

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,11 @@
+import * as fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const readContacts = async () => {};
+export const readContacts = async () => {
+  try {
+    const contacts = await fs.readFile(PATH_DB, { encoding: 'utf-8' });
+    return JSON.parse(contacts);
+  } catch (error) {
+    console.error(error);
+  }
+};
