e48b5bc2f4ca536c21c7259a8f5dc3772ae84b55
ptrnk0 <vlad.ptrnko@gmail.com>
2025-02-21 12:30:55 +0200

    Implement addOneContact function to create and write a fake contact

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,8 @@
-export const addOneContact = async () => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const addOneContact = async () => {
+  await writeContacts([createFakeContact(1)]);
+};
 
 addOneContact();
