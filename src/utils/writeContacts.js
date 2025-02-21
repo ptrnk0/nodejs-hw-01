971edabeba8716e0c5e518c64d9ec724f81babae
ptrnk0 <vlad.ptrnko@gmail.com>
2025-02-21 12:59:23 +0200

    Refactor writeContacts to directly write updated contacts without reading existing ones

--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,14 +1,9 @@
 import { PATH_DB } from '../constants/contacts.js';
 import * as fs from 'node:fs/promises';
-import { readContacts } from './readContacts.js';
 
 export const writeContacts = async (updatedContacts) => {
   try {
-    const contacts = await readContacts();
-    await fs.writeFile(
-      PATH_DB,
-      JSON.stringify([...contacts, ...updatedContacts]),
-    );
+    await fs.writeFile(PATH_DB, JSON.stringify(updatedContacts));
   } catch (error) {
     console.error(error);
   }
